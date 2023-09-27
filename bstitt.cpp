06046ad7eb489627685ff571ff7592725926b903
Braden S <jst1braden@gmail.com>
2023-09-27 09:18:20 -0700

    Added functions to spawn platform at random

--- bstitt.cpp
+++ bstitt.cpp
@@ -10,8 +10,19 @@
 #include <vector>
 #include "defs.h"
 #include "log.h"
+#include "bstitt.h"
 
+#ifdef
 using namespace std;
+
+void display_name(int x, int y, const char *name)
+{
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ff0000, "%s", name);
+}
 
 void display_border(int xres, int yres)
 {
@@ -36,6 +47,42 @@ void display_border(int xres, int yres)
 
     glVertex2i(0 + b, 0 + b);
 
+    glEnd();
+    glPopMatrix();
+}
+
+Platform::void draw_platform()
+{
+    // Draw the platform
+    glPushMatrix();
+    glColor3ub(90, 140, 90);
+    glTranslatef(platform.pos[0], platform.pos[1], platform.pos[2]);
+    glBegin(GL_QUADS);
+    glVertex2i(-platform.width, 0);
+    glVertex2i(-platform.width, platform.height);
+    glVertex2i(platform.width, platform.height);
+    glVertex2i(platform.width, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+Platform::void spawnPlatformRandom()
+{
+    // Spawn a platform at a random location
+    platform.pos[0] = rand() % 600 + 100; // 100 - 700
+    platform.pos[1] = rand() % 400 + 100; // 100 - 500
+    platform.width = 50.0f;
+    platform.height = 8.0f;
+
+    // Draw the platform
+    glPushMatrix();
+    glColor3ub(90, 140, 90);
+    glTranslatef(platform.pos[0], platform.pos[1], platform.pos[2]);
+    glBegin(GL_QUADS);
+    glVertex2i(-platform.width, 0);
+    glVertex2i(-platform.width, platform.height);
+    glVertex2i(platform.width, platform.height);
+    glVertex2i(platform.width, 0);
     glEnd();
     glPopMatrix();
 }
