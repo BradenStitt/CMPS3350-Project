68e59c886b7ccca078aa14f80f99e2ce7784e17d
jrivera1014 <joeybrianr14@gmail.com>
2023-09-29 16:08:11 -0700

    delete display

--- jrivera2.cpp
+++ jrivera2.cpp
@@ -4,20 +4,3 @@
  * 
  **/
 
-#include <X11/Xlib.h>
-#include <unistd.h>
-
-void displayWindow() 
-{
-    Display *d = XOpenDisplay(0);
-
-    if (d) {
-        Window w = XCreateWindow(d, DefaultRootWindow(d), 0, 0, 200,
-                                 100, 0, CopyFromParent, CopyFromParent, 
-                                 CopyFromParent, 0, 0);
-        XMapWindow(d, w);
-        XFlush(d);
-
-        sleep(10);
-    }
-}
