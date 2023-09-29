cfd7f21a55f3be4db2f18e46b924dbe236701e7a
jrivera1014 <joeybrianr14@gmail.com>
2023-09-29 09:49:56 -0700

    background function

--- jrivera2.cpp
+++ jrivera2.cpp
@@ -3,3 +3,21 @@
  * Created:   09-25-2023
  * 
  **/
+
+#include <X11/Xlib.h>
+#include <unistd.h>
+
+void displayWindow() 
+{
+    Display *d = XOpenDisplay(0);
+
+    if (d) {
+        Window w = XCreateWindow(d, DefaultRootWindow(d), 0, 0, 200,
+                                 100, 0, CopyFromParent, CopyFromParent, 
+                                 CopyFromParent, 0, 0);
+        XMapWindow(d, w);
+        XFlush(d);
+
+        sleep(10);
+    }
+}
