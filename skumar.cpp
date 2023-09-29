e8e4ee6849e5256d2909c5117f8d01568b00c41e
kumar7-hub <snehalkumar18@gmail.com>
2023-09-29 09:45:16 -0700

    updated

--- skumar.cpp
+++ skumar.cpp
@@ -18,33 +18,39 @@ using namespace std;
 
 void display_border(int xres, int yres)
 {
-    //Draw a border around the window
+    // Draw a border around the window
     int b = 50;
-	glColor3f(1.0f, 1.0f, 0.0f);
-	glPushMatrix();
-	glBegin(GL_TRIANGLES_STRIP);
-        glVertex2i(0,            0);
-        glVertex2i(0+b         0+b);
-        glVertex2i(0,       0+yres);
-        glVertex2i(0+b,     0+yres-b);
-        glVertex2i(xres     0+yres);
-        glVertex2i(xres-b,     0+yres-b);
-        glVertex2i(xres,     0);
-        glVertex2i(xres-b,     b);
-        glVertex2i(0,     0);
-        glVertex2i(0+b         0+b);
-	glEnd();
-	glPopMatrix();
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
+    glPushMatrix();
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex2i(0, 0);
+    glVertex2i(0 + b, 0 + b);
+    glVertex2i(0, 0 + yres);
+
+    glVertex2i(0 + b, 0 + yres - b);
+    glVertex2i(xres, 0 + yres);
+    glVertex2i(xres - b, 0 + yres - b);
+
+    glVertex2i(xres, 0);
+    glVertex2i(xres - b, b);
+    glVertex2i(0, 0);
+
+    glVertex2i(0 + b, 0 + b);
+
+    glEnd();
+    glPopMatrix();
 }
 
-void display_name(int x, int y)
-{
-    Rect r;
-    r.bot = y;
-    r.left = x;
-    r.center = 0;
-    ggprint8b(&r, 0, 0x00000000, "Snehal");
-}
+// void display_name(int x, int y)
+// {
+//     Rect r;
+//     r.bot = y;
+//     r.left = x;
+//     r.center = 0;
+//     ggprint8b(&r, 0, 0x00000000, "Snehal");
+// }
 
 // Physics for pressing keys and moving the player
 Player::void physics()
