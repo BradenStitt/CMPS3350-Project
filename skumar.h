de6d6340636396f8bb854b17b0ffd988942247c8
kumar7-hub <snehalkumar18@gmail.com>
2023-09-29 09:53:34 -0700

    updated

--- skumar.h
+++ skumar.h
@@ -13,24 +13,10 @@ class Player {
 	float verts[3][2];
 	// float thrust;
 	double angle;
-	Player() {
-		init();
-	}
-	void init() {
-		pos[0] = 100.0f; 
-		pos[1] = 40.0f; 
-
-		vel[0] = vel[1] = 0.0f;
-		//3 vertices of triangle-shaped rocket player
-		verts[0][0] = -10.0f;
-		verts[0][1] =   0.0f;
-		verts[1][0] =   0.0f;
-		verts[1][1] =  30.0f;
-		verts[2][0] =  10.0f;
-		verts[2][1] =   0.0f;
-		angle = 0.0;
-	}
-
-} player;
+	Player();
+	void init();
+	void physics():
+	void draw_player();
+} 
 
 #endif
