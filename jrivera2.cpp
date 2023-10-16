44cc1083be9fbeae4fdce20570fa602d21701827
jrivera1014 <joeybrianr14@gmail.com>
2023-10-16 08:03:48 -0700

    changes to my cpp file

--- jrivera2.cpp
+++ jrivera2.cpp
@@ -4,3 +4,25 @@
  * 
  **/
 
+#include <iostream>
+#include <cmath>
+#include <GL/gl.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include "defs.h"
+#include "log.h"
+#include "bstitt.h"
+#include "global.h"
+
+using namespace std;
+
+void background_display(void)
+{
+
+}
+
--- jrivera2.h
+++ jrivera2.h
@@ -3,3 +3,12 @@
  * Created:   09-25-2023
  * 
  **/
+
+#include <cstdlib> // for rand
+#include <GL/gl.h> 
+
+class Background {
+    public:
+        Display
+    private:
+}
