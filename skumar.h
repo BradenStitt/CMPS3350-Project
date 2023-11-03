4d9a9a43466e1d260a62ae1d421815dc7de0a1fd
kumar7-hub <snehalkumar18@gmail.com>
2023-11-02 22:50:55 -0700

    added additonal variables to classes

--- skumar.h
+++ skumar.h
@@ -20,7 +20,9 @@ class Bullet
 public:
 	Vec pos;
 	Vec vel;
+	bool bulletHit;
 	float color[3];
+	float prevPosY;
 	struct timespec time;
 
 	Bullet();
@@ -38,8 +40,10 @@ public:
 
 	float pos[2];
 	float vel[2];
-	float verts[3][2];
+	float verts[4][2];
+	float width, height;
 	int jumpCount;
+	int enemyDetected;
 	// float thrust;
 	double angle;
 
